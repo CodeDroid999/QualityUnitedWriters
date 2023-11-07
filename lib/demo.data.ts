530dd2272a58f9b01767b54dbfbc2af9d8d85371
CodeDroid99 <johnmwega2002@gmail.com>
2023-11-07 13:01:35 +0300

    create blog pages

--- lib/demo.data.ts
+++ lib/demo.data.ts
@@ -7,51 +7,22 @@ export const description = [
     _key: '9f1a629887fd',
     _type: 'block',
     children: [
-      {
-        _key: '4a58edd077880',
-        _type: 'span',
-        marks: [],
-        text: 'A statically generated blog example using ',
-      },
-      {
+           {
         _key: '4a58edd077881',
         _type: 'span',
         marks: ['ec5b66c9b1e0'],
-        text: 'Next.js',
-      },
-      {
-        _key: '4a58edd077882',
-        _type: 'span',
-        marks: [],
-        text: ' and ',
-      },
-      {
-        _key: '4a58edd077883',
-        _type: 'span',
-        marks: ['1f8991913ea8'],
-        text: 'Sanity',
-      },
-      {
-        _key: '4a58edd077884',
-        _type: 'span',
-        marks: [],
-        text: '.',
+        text: 'Home',
       },
     ],
     markDefs: [
       {
         _key: 'ec5b66c9b1e0',
         _type: 'link',
-        href: 'https://nextjs.org/',
-      },
-      {
-        _key: '1f8991913ea8',
-        _type: 'link',
-        href: 'https://sanity.io/',
-      },
+        href: '/'     },
     ],
     style: 'normal',
   },
 ]
 
-export const ogImageTitle = 'Quality Unitted Writers - Your Academic Research and Project Partner'
+export const ogImageTitle =
+  'Quality Unitted Writers - Your Academic Research and Project Partner'
