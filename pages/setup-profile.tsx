c5bfdb1071a35f145c110caa9208fc8d27b79d7c
CodeDroid99 <johnmwega2002@gmail.com>
2023-11-09 13:23:07 +0300

    practice then perfect

--- pages/setup-profile.tsx
+++ pages/setup-profile.tsx
@@ -96,27 +96,27 @@ export default function CompleteAccount() {
   return (
 
     <div className="mx-auto mt-10 h-screen w-full max-w-[1200px] px-3 md:mt-20">
-      <Head>
+     <Head>
         <title>
-          Airtaska | Get More Done | Post any task. Pick the best person. Get it done. | Post your task for free Earn money as a tasker
+          QualityUnitedWriters - Your Academic Research and Project Partner
         </title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta
           name="description"
-          content="Airtaska is your one-stop destination for finding the right tasks and talented taskers. Post any task, pick the best person, and get it done. Join now to earn money as a tasker or post your tasks for free."
+          content="Discover a dedicated platform for students and tutors offering expert assistance in a wide range of academic research and projects. Quality Unitted Writers connects you with quality solutions for your educational needs. Whether you're seeking help with essays, theses, or any academic work, our talented team is here to assist you."
         />
-        <meta name="keywords" content="Airtaska, tasks, tasker, earn money, post task" />
-        <meta name="author" content="Airtaska" />
+        <meta name="keywords" content="Academic writing services, Expert academic writers, Professional research assistance, High-quality research papers, Academic project support, Thesis and dissertation help, Essay writing service, Top-rated tutors, Academic success tips, Homework assistance, Online tutoring, Quality writing solutions, Best essay writers, Custom research papers, Academic support platform, Tutoring for students, Research paper editing, Writing and editing services, Academic guidance, Homework help for students" />
+        <meta name="author" content="QualityUnitedWriters" />
         <meta name="robots" content="index, follow" />
-        <meta name="og:title" property="og:title" content="Airtaska | Get More Done" />
+        <meta name="og:title" property="og:title" content="QualityUnitedWriters - Your Academic Research and Project Partner" />
         <meta
           name="og:description"
           property="og:description"
-          content="Airtaska is your one-stop destination for finding the right tasks and talented taskers. Post any task, pick the best person, and get it done. Join now to earn money as a tasker or post your tasks for free."
+          content="Discover a dedicated platform for students and tutors offering expert assistance in a wide range of academic research and projects. Quality Unitted Writers connects you with quality solutions for your educational needs. Whether you're seeking help with essays, theses, or any academic work, our talented team is here to assist you."
         />
         <meta name="og:image" property="og:image" content="public/airtaskalogo.jpeg" />
-        <meta name="og:url" property="og:url" content="https://www.QualityUnitedWriters.com" />
+        <meta name="og:url" property="og:url" content="https://www.qualityunitedswriters.com" />
       </Head>
       <div className="mx-auto max-w-[500px]">
         <div className="w-full">
