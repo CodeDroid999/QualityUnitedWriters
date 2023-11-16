b7f6cf522214f135739b11250f9fc863b53d4c41
CodeDroid99 <johnmwega2002@gmail.com>
2023-11-16 09:04:05 +0300

    add highest eraners section

--- components/Become-a-tutor/PostYourAssignment.tsx
+++ components/Become-a-tutor/PostYourAssignment.tsx
@@ -1,48 +1,44 @@
-import Link from 'next/link'
 import React from 'react'
-import { MdArrowForward } from 'react-icons/md'
 
 export default function PostYourAssignment() {
     return (
         <div className="sm:px-18  flex h-[70vh] flex-col-reverse items-center  bg-violet-100  px-4 py-16  lg:px-24 xl:my-10 xl:flex-row  xl:px-36">
-            <div className="flex flex-1 flex-col items-center justify-center">
-                <h1 className="text-center  font-bold text-green-950 md:mt-10 md:mb-5 ">
-                    Find a tutor to help you with your school!
-                </h1>
-                <div className="flex justify-center w-full flex-col md:flex-row md:space-x-6">
-                    <div className="my-2 w-full max-w-sm rounded bg-white  px-1 py-1 sm:w-[150px] xl:my-3   xl:py-4" >
-                        <Link
-                            href="/post-task"
-                            className="flex w-full flex-row items-center justify-center text-md font-semibold text-gray-500"
-                        >
-                            Summary
-                        </Link>
-                    </div>
-                    <div className="my-2 w-full max-w-sm rounded bg-white  px-1 py-1 sm:w-[150px] xl:my-3   xl:py-4" >
-                        <Link
-                            href="/post-task"
-                            className="flex w-full flex-row items-center justify-center text-md font-semibold text-gray-500"
-                        >
-                            Deadline
-                        </Link>
-                    </div>
-                    <div className="my-2 w-full max-w-sm rounded bg-white  px-1 py-1 sm:w-[150px] xl:my-3   xl:py-4" >
-                        <Link
-                            href="/post-task"
-                            className="flex w-full flex-row items-center justify-center text-md font-semibold text-gray-500"
-                        >
-                            Willing to pay
-                        </Link>
-                    </div>
-                    <div className="my-2 w-full max-w-sm rounded bg-yellow-400  px-1 py-1 sm:w-[150px] xl:my-3   xl:py-4" >
-                        <Link
-                            href="/post-task"
-                            className="flex w-full flex-row items-center justify-center text-md font-semibold text-black"
-                        >
-                            Post Homework
-                        </Link>
-                    </div>
+            <div className="container">
+                <div className="flex flex-1 flex-col items-center justify-center">
+                    <h1 className="text-center  font-bold text-yellow-500 md:mt-10 md:mb-5 ">
+                        Why you should tutor on studypool
+                    </h1>
+                    <div className="flex justify-center w-full flex-col md:flex-row md:space-x-6">
+                        <div className="col-md-4">
+                            <div className="p2">
+                                <h3 className="text-blue-950 font-semibold uppercase">
+                                    Lowest service fees
+                                </h3>
+                                <p>
+                                    Starting at 20%, Studypool charges the lowest service fees in the market. We like to give our tutors what they earn
+                                </p>
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <div className="p2">
+                                <h3 className="text-blue-950 font-semibold uppercase">
+                                    Reliable Income
+                                </h3>
+                                <p>
+                                    Make good money using nothing more than your knowledge and your keyboard.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <h3 className="text-blue-950 font-semibold uppercase whitespace-nowrap">
+                                Freedom and Flexibility
+                            </h3>
+                            <p>
+                                Choose when to work and where you want to work from. There is no boss, no office and no quotas. Create your own Schedule.
+                            </p>
+                        </div>
 
+                    </div>
                 </div>
             </div>
         </div>
